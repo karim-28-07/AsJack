e55ee4d2cdb887116f8300d4cc79e933cdc3c670
karim <karim.mezouar@gmail.com>
2021-04-26 17:25:34 +0200

    Suprimer les props dans button.jsx et suprimé des varaibles sur table.js

---
+++ src/Variable.js
@@ -0,0 +1,7 @@
+// const cardSelected2 = this.rndCarte()
+
+// const valueCarte2 = this.transformCardIntoInt(cardSelected2.split("")[0])
+
+// const firstPlayerValue = valueCarte + valueCarte2
+
+    // const firstTwoCardsPlayer = [cardSelected, cardSelected2]
